4530fc244c03a1799f0f00ed0e25dac4786325a8
clementthomas <clement@cotyl.net>
2023-09-19 09:37:43 +0200

    [ADD] survey_event_registration_generation_attachment: add attachment to survey generated event registration

---
+++ survey_event_registration_generation_survey_contact_generation/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2016-2020 Akretion France (<https://www.akretion.com>)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Survey event registration generation",
+    "version": "16.0.0.0.0",
+    "license": "AGPL-3",
+    "author": "Elabore",
+    "website": "https://www.elabore.coop",
+    "category": "",
+    "depends": ["survey_contact_generation","survey_event_registration_generation"],
+    "data": [
+        
+    ],
+    "installable": True,
+    "auto_install":True, 
+}
