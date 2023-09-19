affaf785ee4dcbba08130e8699a22636bb0c3d3a
clementthomas <clement@cotyl.net>
2023-09-19 15:28:26 +0200

    [ADD] survey_event_speaker_generation

---
+++ survey_event_speaker_generation/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2016-2020 Akretion France (<https://www.akretion.com>)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Survey event speaker generation",
+    "version": "16.0.0.0.0",
+    "license": "AGPL-3",
+    "author": "Elabore",
+    "website": "https://www.elabore.coop",
+    "category": "",
+    "depends": ["survey", "event_speaker"],
+    "data": [        
+        'views/mail_templates_chatter.xml',         
+        'views/survey_survey_views.xml',        
+    ],    
+    "installable": True,
+}
---
+++ survey_event_speaker_generation/models/survey_user_input.py
@@ -0,0 +1,44 @@
+
+import logging
+import textwrap
+import uuid
+
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, _, Command
+from odoo.exceptions import ValidationError
+from odoo.tools import float_is_zero
+
+_logger = logging.getLogger(__name__)
+
+
+class SurveyUserInput(models.Model):
+    _inherit = 'survey.user_input'
+
+    speaker_id = fields.Many2one('res.partner', 'Event speaker')
+    
+    def _get_event(self):
+        for line in self.user_input_line_ids:
+            if line.question_id.question_type == 'event' and not line.skipped \
+                and line.answer_type != "suggestion" \
+                    and line.question_id.event_registration_field.name != "comment":
+                return line.value_event
+                
+
+    def _create_speaker_post_process(self, speaker):
+        speaker.message_post_with_view(
+            "survey_event_speaker_generation.message_event_speaker_assigned",
+            values={"speaker": speaker, "user_input": self, "event":self._get_event()},
+            subtype_id=self.env.ref("mail.mt_note").id,
+        )
+
+    def _mark_done(self):
+        """Attach partner as speaker of event"""
+
+        res = super()._mark_done()
+        for user_input in self.filtered(lambda r: r.survey_id.generate_speaker and not r.speaker_id and r.partner_id):            
+            user_input.update({"speaker_id": user_input.partner_id.id})
+            user_input._get_event().speakers = [Command.link(user_input.speaker_id.id)]
+            user_input._create_speaker_post_process(user_input.speaker_id)
+
+        return res
